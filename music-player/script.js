5d371e2ab66dd81c8933cebab47dbcb1efa466f4
paultulod <paultulod@pm.me>
2022-07-17 23:53:37 +0800

    Fix format

--- music-player/script.js
+++ music-player/script.js
@@ -22,78 +22,78 @@ loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-  title.innerText = song;
-  audio.src = `music/${song}.flac`;
-  cover.src = `images/${song}.jpg`;
+	title.innerText = song;
+	audio.src = `music/${song}.flac`;
+	cover.src = `images/${song}.jpg`;
 }
 
 // Play song
 function playSong() {
-  musicContainer.classList.add('play');
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
 
-  audio.play();
+	audio.play();
 }
 
 // Pause song
 function pauseSong() {
-  musicContainer.classList.remove('play');
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('i.fas').classList.add('fa-play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
-  audio.pause();
+	audio.pause();
 }
 
 // Previous Song
 function prevSong() {
-  songIndex--;
+	songIndex--;
 
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
 
-  loadSong(songs[songIndex]);
-  playSong();
+	loadSong(songs[songIndex]);
+	playSong();
 }
 
 // Next Song
 function nextSong() {
-  songIndex++;
+	songIndex++;
 
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
 
-  loadSong(songs[songIndex]);
-  playSong();
+	loadSong(songs[songIndex]);
+	playSong();
 }
 
 // Update Progress bar
 function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
+	const { duration, currentTime } = e.srcElement;
+	const progressPercent = (currentTime / duration) * 100;
+	progress.style.width = `${progressPercent}%`;
 }
 
 // Set progress bar
 function setProgress(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const duration = audio.duration;
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
 
-  audio.currentTime = (clickX / width) * duration;
+	audio.currentTime = (clickX / width) * duration;
 }
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play');
+	const isPlaying = musicContainer.classList.contains('play');
 
-  if (isPlaying) {
-    pauseSong();
-  } else {
-    playSong();
-  }
+	if (isPlaying) {
+		pauseSong();
+	} else {
+		playSong();
+	}
 });
 
 // Change song
