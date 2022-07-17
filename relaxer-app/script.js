3787e29d637b1b5ac1e65da42eb3312075c730d6
paultulod <paultulod@pm.me>
2022-07-17 23:55:05 +0800

    Fix format

--- relaxer-app/script.js
+++ relaxer-app/script.js
@@ -8,17 +8,17 @@ const holdTime = totalTime / 5;
 breathAnimation();
 
 function breathAnimation() {
-  text.innerText = 'Breathe In!';
-  container.className = 'container grow';
+	text.innerText = 'Breathe In!';
+	container.className = 'container grow';
 
-  setTimeout(() => {
-    text.innerText = 'Hold';
+	setTimeout(() => {
+		text.innerText = 'Hold';
 
-    setTimeout(() => {
-      text.innerText = 'Breathe Out!';
-      container.className = 'container shrink';
-    }, holdTime);
-  }, breatheTime);
+		setTimeout(() => {
+			text.innerText = 'Breathe Out!';
+			container.className = 'container shrink';
+		}, holdTime);
+	}, breatheTime);
 }
 
 setInterval(breathAnimation, totalTime);
